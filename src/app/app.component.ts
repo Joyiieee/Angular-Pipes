2d3a694ddb967432507e07476472ab0eb7c370d6
Joyiieee <123354370+Joyiieee@users.noreply.github.com>
2024-05-13 10:06:50 +0800

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { EducationComponent } from './education/education.component';
+import { ReferencesComponent } from './references/references.component';
+import { TrainingComponent } from './training/training.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { SkillsCertificationComponent } from './training/skills-certification/skills-certification.component';
+import { PipesComponent } from './pipes/pipes.component';
+
+@Component({
+selector: 'app-root',
+standalone: true,
+imports: [RouterOutlet, PersonalInfoComponent, EducationComponent, ReferencesComponent, TrainingComponent, WorkExperienceComponent, SkillsCertificationComponent,PipesComponent
+],
+templateUrl: './app.component.html',
+styleUrl: './app.component.css'
+})
+export class AppComponent {
+title = 'Resume';
+name: string = 'Clarence Mica Joy G. Tolentino';
+}
